aa460c0b93732e8ce591c406b9c1a03f3794cd76
Dima <dimabojko93@gmail.com>
2022-10-24 13:23:40 +0300

    homework 6

--- homework_5.js
+++ homework_5.js
@@ -3,7 +3,7 @@
 let userMessage = prompt("Введіть ваше повідомлення (не менше 10 символів)");
     
 for (let i = 0; i < 1; i++) {
-    if (userMessage == null || userMessage == undefined || userMessage == "" || userMessage == " " || userMessage.length < 10) {
+    if (userMessage == null || userMessage == undefined || userMessage == "" || userMessage.length < 10) {
         alert("Ви ввели повідомлення менше 10 символів")
         userMessage = prompt("Введіть ваше повідомлення (не менше 10 символів)");
         i = -1;
---
+++ homework_6.js
@@ -0,0 +1,6 @@
+"use strict";
+
+let a = 1;
+let b = 4;
+
+let result = a + b < 4 ?  'Мало' : 'Багато'
