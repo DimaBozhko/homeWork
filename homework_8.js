e0ffc2a629771c5c19f6ee48ced44e1d69340521
Dima <dimabojko93@gmail.com>
2022-10-24 13:57:38 +0300

    homework 8

---
+++ homework_8.js
@@ -0,0 +1,14 @@
+"use strict";
+
+let age = 14;
+let isAge = age >= 14 && age <=90;
+
+// більш розгорнутий варіант
+
+// if (age >= 14 && age <=90) {
+//    isAge = true;
+// } else {
+//    isAge = false;
+// }
+
+console.log(isAge);
