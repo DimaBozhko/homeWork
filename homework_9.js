7672ad7adba62ba4d008651a952c21de8f85636f
Dima <dimabojko93@gmail.com>
2022-10-28 19:46:27 +0300

    homework 9

---
+++ homework_9.js
@@ -0,0 +1,18 @@
+"use strict";
+
+const pow = (x, n) => x ** n;
+
+// ------------------------- Arrow Function
+// const pow = (x, n) => { 
+//    return x ** n;
+// }
+// ------------------------- Function Declaration
+// function pow(x, n) {
+//     return x ** n
+// }
+// ------------------------- Function Expression
+//const pow = function (x, n) {
+//     return x ** n
+// }
+
+console.log(pow(3, 2))
