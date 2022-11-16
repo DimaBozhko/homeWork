815fbf24c41418fe1efb9bf956c02fc15458f9d2
Dima <dimabojko93@gmail.com>
2022-11-16 17:43:06 +0200

    homework 18

---
+++ homework_18.js
@@ -0,0 +1,13 @@
+"use strict";
+
+function getData() {
+
+    fetch("https://jsonplaceholder.typicode.com/posts")
+        .then((data) => data.json())
+        .then((data) => data.map(item => Object.fromEntries(Object.entries(item).filter(([key]) => key.includes('userId')))))
+        .then((data) => console.log(data))
+
+}
+
+getData();
+
