c3db1927f4400fd749401fce1f4a758f1acf313e
Dima <dimabojko93@gmail.com>
2023-01-18 11:00:49 +0200

    homework 27

--- homework_27.js
+++ homework_27.js
@@ -3,8 +3,12 @@
 // Створіть функцію unique(arr), яка поверне масив унікальних значень масиву arr, що не повторюються.
 
 function unique(arr) {
-    const filteredArr = arr.filter((item, index) => arr.indexOf(item) === index);
-    return filteredArr;
+    
+    // const filteredArr = arr.filter((item, index) => arr.indexOf(item) === index);
+    
+    const filteredArr = new Set(arr);
+    return [...filteredArr];
+
 }
    
 let values = ["Hare", "Krishna", "Hare", "Krishna", "Krishna", "Krishna", "Hare", "Hare", ":-O"];
